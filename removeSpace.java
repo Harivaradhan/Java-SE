8fe1c36dac9b665ca092e91c7f352889d3bed305
jack10 <76940581+Harivaradhan@users.noreply.github.com>
2022-11-12 19:39:49 +0530

    Create removeSpace.java

---
+++ removeSpace.java
@@ -0,0 +1,9 @@
+public static void main(String[] args)
+{
+	//check the number is binary or not
+	String str="ad   fef   54 gr    bew";
+	System.out.println(str);
+	String str2= str.replaceAll("[\\s]*"," ");
+	System.out.print(str2);
+	}
+	}
