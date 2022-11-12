6eea549d3bd7a44ab5b4e705f0184f15f4c4817e
jack10 <76940581+Harivaradhan@users.noreply.github.com>
2022-11-12 18:25:25 +0530

    Create binarycheckString.java

---
+++ binarycheckString.java
@@ -0,0 +1,10 @@
+public static void main(String[] args)
+{
+	//check the number is binary or not
+	long b=101011;
+	String str=String.valueOf(b);
+	System.out.println(str);
+	System.out.println(str.matches("[01]*"));
+
+	}
+	}
