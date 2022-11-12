f48995f7db790526126e4362a02d2c653698413c
jack10 <76940581+Harivaradhan@users.noreply.github.com>
2022-11-12 18:30:22 +0530

    Create checkhexadecimalnum.java

---
+++ checkhexadecimalnum.java
@@ -0,0 +1,9 @@
+public static void main(String[] args)
+{
+	//check the number is binary or not
+	String str="ad345";
+	System.out.println(str);
+	System.out.println(str.matches("[0-9a-f]*"));
+
+	}
+	}
